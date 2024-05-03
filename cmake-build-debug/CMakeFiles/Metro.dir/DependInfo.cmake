97e193e7a13e897d5c17d98266b080892168928b
iseltarek <iseltarek15@gmail.com>
2024-05-03 18:43:51 +0300

    subscription

---
+++ cmake-build-debug/CMakeFiles/Metro.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/pc/CLionProjects/Metro-Management-System/Statistics.cpp" "CMakeFiles/Metro.dir/Statistics.cpp.obj" "gcc" "CMakeFiles/Metro.dir/Statistics.cpp.obj.d"
+  "C:/Users/pc/CLionProjects/Metro-Management-System/line.cpp" "CMakeFiles/Metro.dir/line.cpp.obj" "gcc" "CMakeFiles/Metro.dir/line.cpp.obj.d"
+  "C:/Users/pc/CLionProjects/Metro-Management-System/main.cpp" "CMakeFiles/Metro.dir/main.cpp.obj" "gcc" "CMakeFiles/Metro.dir/main.cpp.obj.d"
+  "C:/Users/pc/CLionProjects/Metro-Management-System/metro.cpp" "CMakeFiles/Metro.dir/metro.cpp.obj" "gcc" "CMakeFiles/Metro.dir/metro.cpp.obj.d"
+  "C:/Users/pc/CLionProjects/Metro-Management-System/station.cpp" "CMakeFiles/Metro.dir/station.cpp.obj" "gcc" "CMakeFiles/Metro.dir/station.cpp.obj.d"
+  "C:/Users/pc/CLionProjects/Metro-Management-System/subscription.cpp" "CMakeFiles/Metro.dir/subscription.cpp.obj" "gcc" "CMakeFiles/Metro.dir/subscription.cpp.obj.d"
+  "C:/Users/pc/CLionProjects/Metro-Management-System/wallet.cpp" "CMakeFiles/Metro.dir/wallet.cpp.obj" "gcc" "CMakeFiles/Metro.dir/wallet.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Metro.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Metro.
+# This may be replaced when dependencies are built.

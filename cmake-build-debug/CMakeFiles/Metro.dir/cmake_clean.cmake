63f5809fd6e958ef071bfd28310529cefc453e90
omarWael000 <owael4368@gmail.com>
2024-05-04 21:23:31 +0300

    omar wael

---
+++ cmake-build-debug/CMakeFiles/Metro.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Metro.dir/Statistics.cpp.obj"
+  "CMakeFiles/Metro.dir/Statistics.cpp.obj.d"
+  "CMakeFiles/Metro.dir/line.cpp.obj"
+  "CMakeFiles/Metro.dir/line.cpp.obj.d"
+  "CMakeFiles/Metro.dir/main.cpp.obj"
+  "CMakeFiles/Metro.dir/main.cpp.obj.d"
+  "CMakeFiles/Metro.dir/metro.cpp.obj"
+  "CMakeFiles/Metro.dir/metro.cpp.obj.d"
+  "CMakeFiles/Metro.dir/station.cpp.obj"
+  "CMakeFiles/Metro.dir/station.cpp.obj.d"
+  "Metro.exe"
+  "Metro.exe.manifest"
+  "Metro.pdb"
+  "libMetro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Metro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
